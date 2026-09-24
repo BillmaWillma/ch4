11c3c7bcaf9122669d00279851b2f8044422da8e
BillmaWillma <williamwu112813@gmail.com>
2026-09-24 13:01:19 -0700

    Add files via upload

--- Multadd.java
+++ Multadd.java
@@ -1,5 +1,6 @@
 public class Multadd {
 	
+	
 	public static double multadd (double a, double b, double c) {
 		return a*b+c ;
 		
@@ -11,13 +12,14 @@ public class Multadd {
 		
 	}	
 	
+	
 	public static void main(String[] args){
 		System.out.println(multadd(1.0, 2.0, 3.0)) ;
 		
 		System.out.println(multadd(0.5,  Math.cos(Math.PI / 4), Math.sin(Math.PI / 4) ) ) ;
 		System.out.println(multadd(1.0, Math.log10(10),  Math.log10(20) ) ) ;
 		
-		System.out.println(expSum (2) ) ;
+		System.out.println(expSum (2.0) ) ;
 		
 		
 	}
